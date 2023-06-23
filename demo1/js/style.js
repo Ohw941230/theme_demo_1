1b8cbe84365efaa3814d507c0e2b4e8111c737fd
Ohw941230 <134983388+Ohw941230@users.noreply.github.com>
2023-06-23 15:46:19 +0900

    Add files via upload

--- demo1/js/style.js
+++ demo1/js/style.js
@@ -1,3 +1,13 @@
 function changeImage(anything) {
   document.getElementById("slider").src = anything;
 }
+
+$(window).bind("scroll", function () {
+  scrollHeight = $(document).height();
+  scrollPosition = $(window).height() + $(window).scrollTop();
+  if ((scrollHeight - scrollPosition) / scrollHeight <= 0.05) {
+    $(".copy_none").addClass("dis_none");
+  } else {
+    $(".copy_none").removeClass("dis_none");
+  }
+});
